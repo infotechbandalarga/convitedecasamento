cfa7803da6a080c6322465ad066b3715b2ca19b9
infotech banda larga <coodenext@gmail.com>
2024-10-23 15:45:05 -0300

    alteração de código
    alterado

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 function openEnvelope() {
-  const envelope = document.getElementById('envelope');
-  const videoContainer = document.getElementById('videoContainer');
-
-  // Esconder o envelope
-  envelope.style.display = 'none';
-
-  // Mostrar o vídeo
-  videoContainer.style.display = 'flex'; // Alterado para 'flex' para melhor centralização
-}
-
-function closeVideo() {
-    console.log('Botão de voltar clicado'); // Para verificar se a função é chamada
+    const envelope = document.getElementById('envelope');
+    const videoContainer = document.getElementById('videoContainer');
+  
+    // Esconder o envelope
+    envelope.style.display = 'none';
+  
+    // Mostrar o vídeo
+    videoContainer.style.display = 'flex'; // Manter como 'flex' para centralização
+  }
+  
+  function closeVideo() {
     const envelope = document.getElementById('envelope');
     const videoContainer = document.getElementById('videoContainer');
   
@@ -19,4 +18,5 @@ function closeVideo() {
   
     // Esconder o vídeo
     videoContainer.style.display = 'none';
-  }  
+  }
+  
